d04561d47d86f6fb9be9d20854350d34cc941569
Your Name <delchoatanasov11@abv.bg>
2024-05-08 09:22:11 +0300

    gadlike

---
+++ PetShelter_StepbyStep_Guide/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PetShelter_StepbyStep_Guide.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
