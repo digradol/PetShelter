9756cc5d2ca3623b189c00f20064576952a590a2
Your Name <delchoatanasov11@abv.bg>
2024-05-09 09:35:31 +0300

    newest project

--- PetShelter.Shared/Repos/Contracts/IBaseRepository.cs
+++ PetShelter.Shared/Repos/Contracts/IBaseRepository.cs
@@ -10,13 +10,13 @@ namespace PetShelter.Shared.Repos.Contracts
     public interface IBaseRepository<TModel>
         where TModel : BaseModel
     {
-        Task<IEnumerable<TModel>> GetAllAsyns();
-        Task<TModel> GetByIdAsyns(int id);
-        Task CreateAsyns(TModel model);
-        Task UpdateAsyns(TModel model);
-        Task SaveAsyns(TModel model);
-        Task DeleteAsyns(int id);
-        Task<bool> ExistsByIdAsyns(int id);
-        Task<IEnumerable<TModel>> GetWithPaginationAsyns(int pageSize, int pageNumber);
+        Task<IEnumerable<TModel>> GetAllAsync();
+        Task<TModel> GetByIdAsync(int id);
+        Task CreateAsync(TModel model);
+        Task UpdateAsync(TModel model);
+        Task SaveAsync(TModel model);
+        Task DeleteAsync(int id);
+        Task<bool> ExistsByIdAsync(int id);
+        Task<IEnumerable<TModel>> GetWithPaginationAsync(int pageSize, int pageNumber);
     }
 }
